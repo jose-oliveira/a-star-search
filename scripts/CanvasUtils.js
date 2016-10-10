c5715514a46984299f4dfd70db847d989dce16f0
Jose Moreira <jose.moreira@acquia.com>
2016-10-09 21:02:51 -0400

    Increasing point radius.

--- scripts/CanvasUtils.js
+++ scripts/CanvasUtils.js
@@ -1,11 +1,11 @@
 class CanvasUtils{
-  
+
   static get CANVASCONSTS () {
     const canvas = document.querySelector("#path canvas");
     return {
       CANVAS: canvas,
       CTX: canvas.getContext("2d"),
-      POINTRADIUS: 5,
+      POINTRADIUS: 10,
       INITIALCOLOR: '#000',
       VISITEDCOLOR: '#F00',
       ANIMTIME: 1000
